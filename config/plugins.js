b477f24394588121b99de25d48e121b7ead5db29
Wendy <111409717+wendy-aik-maung@users.noreply.github.com>
2023-01-23 16:15:20 +0630

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,31 @@
+// path: ./config/plugins.js
+
+module.exports = ({ env }) => ({
+    //
+    graphql: {
+        config: {
+            endpoint: "/graphql",
+            shadowCRUD: true,
+            playgroundAlways: false,
+            depthLimit: 7,
+            amountLimit: 100,
+            apolloServer: {
+                tracing: false,
+            },
+        },
+    },
+    upload: {
+        config: {
+            provider: "cloudinary",
+            providerOptions: {
+                cloud_name: env("CLOUDINARY_NAME"),
+                api_key: env("CLOUDINARY_KEY"),
+                api_secret: env("CLOUDINARY_SECRET"),
+            },
+            actionOptions: {
+                upload: {},
+                delete: {},
+            },
+        },
+    },
+});
